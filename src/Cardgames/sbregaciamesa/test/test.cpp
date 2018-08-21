184a638f11df72a5c164506f99ce3a6f9d0ebc6d
Matteo Paoluzzi <matteo.paoluzzi.ext@leonardocompany.com>
2018-08-21 15:10:15 +0200

    Update sbregaciamesa con anche analisi del grafo finale. E' molto affamato di RAM...

--- src/Cardgames/sbregaciamesa/test/test.cpp
+++ src/Cardgames/sbregaciamesa/test/test.cpp
@@ -1,4 +1,5 @@
 #include <iostream>
+#include <cstdio>
 #include <map>
 #include <set>
 
@@ -8,6 +9,7 @@ class Nodo
 {
   std::string _name;
 public:
+  bool _visited;
   std::set<unsigned int> _partite;
   std::set < Nodo* > _ingressi;
   std::set < Nodo* > _uscite;
@@ -15,6 +17,7 @@ public:
   Nodo()
   {
     _name = "";
+    _visited = false;
     _partite.clear();
     _ingressi.clear();
     _uscite.clear();
@@ -33,6 +36,51 @@ public:
     _uscite.clear();
   }
 };
+typedef std::set < Nodo* > insieme_nodi_t;
+typedef insieme_nodi_t::iterator insieme_nodi_it;
+
+bool parse_node(Nodo* radix, insieme_nodi_t &stack_ricorsione)
+{
+  if (!radix->_visited)
+  {
+    // marco il nodo corrente come visitato e lo aggiungo allo stack di ricorsione
+    radix->_visited = true;
+    stack_ricorsione.insert(radix);
+
+    // ricorro in tutti i vertici adiacenti
+    // uscite
+    for (insieme_nodi_it it = radix->_uscite.begin(); it != radix->_uscite.end(); ++it)
+    {
+      Nodo *nodo = (*it);
+      if (!nodo->_visited && parse_node(nodo, stack_ricorsione))
+      {
+        return true;
+      }
+      else if (stack_ricorsione.find(nodo) != stack_ricorsione.end())
+      {
+        return true;
+      }
+    }
+
+  }
+  stack_ricorsione.erase(radix);
+  return false;
+}
+
+void print_spinning()
+{
+  static unsigned int count = 0;
+  switch (count)
+  {
+    case 0: printf("\b|"); break;
+    case 1: printf("\b/"); break;
+    case 2: printf("\b-"); break;
+    case 3: printf("\b\\"); break;
+    default:
+      count = 0;
+  }
+  fflush(stdout);
+}
 
 int main(int argc, char *argv[])
 {
@@ -43,6 +91,8 @@ int main(int argc, char *argv[])
     num_partite = atoi(argv[1]);
   }
   double results[4] = { 0 }; // 0: # loops, 1: # vittorie 1, 2: # vittore 2, 3: # errori
+  std::cout << "Partite in corso" << std::endl;
+
   for (unsigned int partita = 0; partita < num_partite; partita++)
   {
     Sbregaciamesa nuovo_gioco;
@@ -53,7 +103,7 @@ int main(int argc, char *argv[])
     while (!old_match_found && !vincitore)
     {
       std::string stringa_inizio = nuovo_gioco.to_string();
-
+      //print_spinning();
       vincitore = nuovo_gioco.play_hand(mano);
 
       std::string stringa_fine = nuovo_gioco.to_string();
@@ -97,6 +147,7 @@ int main(int argc, char *argv[])
       fine->_partite.insert(partita);
     }
     results[vincitore]++;
+    std::cout << ".";
     /*
     std::cout << "Partita " << partita + 1 << " : ";
     if (vincitore != 0)
@@ -110,10 +161,34 @@ int main(int argc, char *argv[])
     */
   }
 
+  std::cout << std::endl;
   std::cout << "Loops semplici : " << results[0] << std::endl;
   std::cout << "Vittorie Giocatore 1 : " << results[1] << " ( " << ((results[1] / num_partite) * 100.0) << " % )" << std::endl;
   std::cout << "Vittorie Giocatore 2 : " << results[2] << " ( " << ((results[2] / num_partite) * 100.0) << " % )" << std::endl;
   std::cout << "Errori : " << results[3] << std::endl;
 
+  std::cout << "Avvio ricerca di loop complessi..." << std::endl;
+
+  // cerco le radici
+  insieme_nodi_t radixes;
+  for (std::map<std::string, Nodo>::iterator nodo = mani.begin(); nodo != mani.end(); ++nodo)
+  {
+    if (nodo->second._ingressi.size() == 0)
+    {
+      radixes.insert(&nodo->second);
+    }
+  }
+
+  // parso il grafo
+  for (insieme_nodi_it radice = radixes.begin(); radice != radixes.end(); ++radice)
+  {
+    insieme_nodi_t nodi;
+    if (parse_node(*radice, nodi))
+    {
+      std::cout << "Ho trovato un loop" << std::endl;
+    }
+  }
+
   return 0;
 }
+

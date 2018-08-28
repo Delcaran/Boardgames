d020ebb345dc9eac5b122cb043c1c3af3832b094
Matteo Paoluzzi <matteo.paoluzzi.ext@leonardocompany.com>
2018-08-28 15:55:01 +0200

    Aggiunto un esempio di grafo orientato in QT, da usare per implementare la mia GUI.

--- src/Cardgames/sbregaciamesa/test/main.cpp
+++ src/Cardgames/sbregaciamesa/test/main.cpp
@@ -1,48 +1,64 @@
+/****************************************************************************
+**
+** Copyright (C) 2016 The Qt Company Ltd.
+** Contact: https://www.qt.io/licensing/
+**
+** This file is part of the examples of the Qt Toolkit.
+**
+** $QT_BEGIN_LICENSE:BSD$
+** Commercial License Usage
+** Licensees holding valid commercial Qt licenses may use this file in
+** accordance with the commercial license agreement provided with the
+** Software or, alternatively, in accordance with the terms contained in
+** a written agreement between you and The Qt Company. For licensing terms
+** and conditions see https://www.qt.io/terms-conditions. For further
+** information use the contact form at https://www.qt.io/contact-us.
+**
+** BSD License Usage
+** Alternatively, you may use this file under the terms of the BSD license
+** as follows:
+**
+** "Redistribution and use in source and binary forms, with or without
+** modification, are permitted provided that the following conditions are
+** met:
+**   * Redistributions of source code must retain the above copyright
+**     notice, this list of conditions and the following disclaimer.
+**   * Redistributions in binary form must reproduce the above copyright
+**     notice, this list of conditions and the following disclaimer in
+**     the documentation and/or other materials provided with the
+**     distribution.
+**   * Neither the name of The Qt Company Ltd nor the names of its
+**     contributors may be used to endorse or promote products derived
+**     from this software without specific prior written permission.
+**
+**
+** THIS SOFTWARE IS PROVIDED BY THE COPYRIGHT HOLDERS AND CONTRIBUTORS
+** "AS IS" AND ANY EXPRESS OR IMPLIED WARRANTIES, INCLUDING, BUT NOT
+** LIMITED TO, THE IMPLIED WARRANTIES OF MERCHANTABILITY AND FITNESS FOR
+** A PARTICULAR PURPOSE ARE DISCLAIMED. IN NO EVENT SHALL THE COPYRIGHT
+** OWNER OR CONTRIBUTORS BE LIABLE FOR ANY DIRECT, INDIRECT, INCIDENTAL,
+** SPECIAL, EXEMPLARY, OR CONSEQUENTIAL DAMAGES (INCLUDING, BUT NOT
+** LIMITED TO, PROCUREMENT OF SUBSTITUTE GOODS OR SERVICES; LOSS OF USE,
+** DATA, OR PROFITS; OR BUSINESS INTERRUPTION) HOWEVER CAUSED AND ON ANY
+** THEORY OF LIABILITY, WHETHER IN CONTRACT, STRICT LIABILITY, OR TORT
+** (INCLUDING NEGLIGENCE OR OTHERWISE) ARISING IN ANY WAY OUT OF THE USE
+** OF THIS SOFTWARE, EVEN IF ADVISED OF THE POSSIBILITY OF SUCH DAMAGE."
+**
+** $QT_END_LICENSE$
+**
+****************************************************************************/
+
+
+#include "graphwidget.h"
+#include <sbregaciamesa.h>
+
+#include <QApplication>
+#include <QTime>
+#include <QMainWindow>
+
 #include <iostream>
-#include <cstdio>
-#include <map>
-#include <set>
-#include <list>
 
-#include <Sbregaciamesa.h>
-
-class Vertex
-{
-  std::string _name;
-public:
-  bool _visited;
-  std::set<unsigned int> _partite;
-  std::set<Vertex*> _input; // genitori
-  std::set<Vertex*> _output; // figli
-
-  Vertex()
-  {
-    _name = "";
-    _visited = false;
-    _partite.clear();
-    _input.clear();
-    _output.clear();
-  }
-  Vertex(const std::string &name) : _name(name)
-  {
-    _partite.clear();
-    _input.clear();
-    _output.clear();
-  }
-
-  ~Vertex()
-  {
-    _partite.clear();
-    _input.clear();
-    _output.clear();
-  }
-
-  bool Vertex::operator==(const Vertex &v) const
-  {
-    return _name == v._name;
-  }
-};
-
+/*
 bool has_loops(std::map<std::string, Vertex> &vertici)
 {
   // Sfrutto l'algoritmo di Kahn per l'ordinamento topologico per individuare cicli
@@ -81,10 +97,12 @@ bool has_loops(std::map<std::string, Vertex> &vertici)
   }
   return false;
 }
+*/
 
-int main(int argc, char *argv[])
+int main(int argc, char **argv)
 {
-  std::map<std::string, Vertex> mani;
+    // prima gioco le partite
+    std::map<std::string, Vertex> mani;
   unsigned int num_partite = 10;
   if (argc > 1)
   {
@@ -103,13 +121,9 @@ int main(int argc, char *argv[])
     while (!old_match_found && !vincitore)
     {
       std::string stringa_inizio = nuovo_gioco.to_string();
-      //print_spinning();
       vincitore = nuovo_gioco.play_hand(mano);
-
+      /*
       std::string stringa_fine = nuovo_gioco.to_string();
-      std::map<std::string, Vertex>::iterator itin = mani.find(stringa_inizio);
-      std::map<std::string, Vertex>::iterator itfi = mani.find(stringa_fine);
-      Vertex *inizio, *fine;
       if (itin != mani.end())
       {
         // questa mano era gia' presente: prendo l'indirizzo
@@ -145,21 +159,12 @@ int main(int argc, char *argv[])
       inizio->_partite.insert(partita);
       fine->_input.insert(inizio);
       fine->_partite.insert(partita);
+      */
     }
     results[vincitore]++;
     std::cout << ".";
-    /*
-    std::cout << "Partita " << partita + 1 << " : ";
-    if (vincitore != 0)
-    {
-      std::cout << "vince " << vincitore << " dopo " << mano << " mani" << std::endl;
-    }
-    if (old_match_found)
-    {
-      std::cout << "trovata una mano gia' giocata dopo " << mano << " mani, termino" << std::endl;
-    }
-    */
   }
+  // ora disegno i nodi
 
   std::cout << std::endl;
   std::cout << "Loop semplici : " << results[0] << std::endl;
@@ -168,12 +173,21 @@ int main(int argc, char *argv[])
   std::cout << "Errori : " << results[3] << std::endl;
 
   std::cout << "Avvio ricerca di loop complessi..." << std::endl;
+  /*
   if (has_loops(mani))
   {
     std::cout << "Esiste almeno una partita infinita" << std::endl;
   }
+  */
 
-  
-  return 0;
+
+    QApplication app(argc, argv);
+
+    GraphWidget *widget = new GraphWidget;
+
+    QMainWindow mainWindow;
+    mainWindow.setCentralWidget(widget);
+
+    mainWindow.show();
+    return app.exec();
 }
-
